7eff0bb6373d86a9f62adf4570a8ebdf2d94fba4
Mathieu Souchaud <mattieu.souchaud@free.fr>
2015-01-06 11:32:38 +0100

    Add listview.

--- app/src/main/java/souch/smp/Main.java
+++ app/src/main/java/souch/smp/Main.java
@@ -1,11 +1,18 @@
 package souch.smp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Main extends Activity {
@@ -15,7 +22,64 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         //getSupportActionBar().setDisplayShowHomeEnabled(true);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+
+        //On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> map;
+        map = new HashMap<String, String>();
+        map.put("tab", " ");
+        map.put("desc", "Folder 1");
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("tab", "   ");
+        map.put("desc", "Yo");
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("tab", " ");
+        map.put("desc", "Folder 2");
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("tab", "   ");
+        map.put("desc", "Yup");
+        listItem.add(map);
+
+        for(int i = 0; i < 100; i++) {
+            map = new HashMap<String, String>();
+            map.put("tab", "   ");
+            map.put("desc", "Yup " + i);
+            listItem.add(map);
+         }
+
+        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.item_listview,
+               new String[] {"tab", "desc"}, new int[] {R.id.tab, R.id.desc});
+
+        listView.setAdapter(mSchedule);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                //on récupère la HashMap contenant les infos de notre item (titre, description, img)
+                HashMap<String, String> map = (HashMap<String, String>) listView.getItemAtPosition(position);
+                //on créé une boite de dialogue
+                AlertDialog.Builder adb = new AlertDialog.Builder(Main.this);
+                //on attribue un titre à notre boite de dialogue
+                adb.setTitle("Sélection Item");
+                //on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
+                adb.setMessage("Votre choix : " + map.get("desc"));
+                //on indique que l'on veut le bouton ok à notre boite de dialogue
+                adb.setPositiveButton("Ok", null);
+                //on affiche la boite de dialogue
+                adb.show();
+            }
+        });
     }
+
 
 
     @Override
